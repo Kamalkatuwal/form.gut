8cca4c7d557a8ede056610cad72def1cb31671dc
kamalkatuwal <bkatuwal23@gmail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase falsegit config --get user.namegit config --get user.email.exitlsspfkgpfffgit exi>
2023-08-05 18:50:51 +0545

    updating my code

---
+++ form.js
@@ -0,0 +1,28 @@
+let a=document.getElementById("sign-upform");
+let b=document.getElementById("hdr");
+let element=document.getElementById("we");
+element.addEventListener("click",enable);
+//check function
+function myFunction() {
+    var x = document.getElementById("passw");
+    var y= document.getElementById('radio')
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+      y.checked=false;
+
+    }
+  }
+  //animation enable function
+  function enable(){
+    a.style.animationPlayState="running";
+    b.style.animationPlayState="running";
+    document.body.style.backgroundColor = "#b7fa42";
+
+  }
+ function reve(){
+  a.style.animation="side 2s ease-in-out 0.2s 1 reverse backwards";
+  b.style.animation="side-side 2s ease-in-out 0.2s 1 reverse backwards"
+  document.body.style.backgroundColor = "aqua";
+ }
